75e17981fa52373638b3a67e0ea2f125d495c2e4
Plamen Popov <tzappa@gmail.com>
2014-12-17 17:56:45 +0200

    array() instead of []

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package    SugiPHP
- * @subpackage Routing
- * @category   tests
- * @author     Plamen Popov <tzappa@gmail.com>
- * @license    http://opensource.org/licenses/mit-license.php (MIT License)
+ * PHP Unit tests for Router class
+ *
+ * @package SugiPHP.Routing
+ * @author  Plamen Popov <tzappa@gmail.com>
+ * @license http://opensource.org/licenses/mit-license.php (MIT License)
  */
 
 namespace SugiPHP\Routing;
@@ -41,21 +41,21 @@ class RouterTest extends \PHPUnit_Framework_TestCase
 		$router->add("article", new Route("/show/{title}"));
 		$router->add("mvc", new Route("/{controller}/{action}/{param}", array("controller" => "home", "action" => "index", "param" => "")));
 
-		$this->assertSame("/", $router->build("home", []));
-		$this->assertSame("/", $router->build("home", ["something" => "else"]));
+		$this->assertSame("/", $router->build("home", array()));
+		$this->assertSame("/", $router->build("home", array("something" => "else")));
 
-		$this->assertFalse($router->build("article", []));
-		$this->assertFalse($router->build("article", ["something" => "else"]));
-		$this->assertFalse($router->build("article", ["title" => ""]));
-		$this->assertFalse($router->build("article", ["title" => false]));
-		$this->assertSame("/show/1", $router->build("article", ["title" => "1"]));
-		$this->assertSame("/show/thisisatitle", $router->build("article", ["title" => "thisisatitle"]));
+		$this->assertFalse($router->build("article", array()));
+		$this->assertFalse($router->build("article", array("something" => "else")));
+		$this->assertFalse($router->build("article", array("title" => "")));
+		$this->assertFalse($router->build("article", array("title" => false)));
+		$this->assertSame("/show/1", $router->build("article", array("title" => "1")));
+		$this->assertSame("/show/thisisatitle", $router->build("article", array("title" => "thisisatitle")));
 
-		$this->assertSame("/", $router->build("mvc", []));
-		$this->assertSame("/", $router->build("mvc", ["foo" => "bar"]));
-		$this->assertSame("/", $router->build("mvc", ["controller" => ""]));
-		$this->assertSame("/", $router->build("mvc", ["controller" => false]));
-		$this->assertSame("/", $router->build("mvc", ["controller" => "home"]));
+		$this->assertSame("/", $router->build("mvc", array()));
+		$this->assertSame("/", $router->build("mvc", array("foo" => "bar")));
+		$this->assertSame("/", $router->build("mvc", array("controller" => "")));
+		$this->assertSame("/", $router->build("mvc", array("controller" => false)));
+		$this->assertSame("/", $router->build("mvc", array("controller" => "home")));
 	}
 
 	public function testBuildWithPathType()
